f0b397209e9e211911575b825ede645710d3d927
Nathanael G <goza.wave@gmail.com>
2020-02-01 14:28:25 -0600

    R4 First Commit
    New Repo for 2020 GGJ

---
+++ Assets/Script/BotControls.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotControls : MonoBehaviour
+{
+    public string[] controls = new string[5];           //Controls used for player  |  Asigned in the Unity Editor
+    public string[] desiredControls = new string[5];    //When a Cp is captured Controls used for player  |  Asigned in the Unity Editor
+    public Rigidbody2D botRB;                           //Rigidbody for position movement 
+    
+    public float maxSpeed = 2.0f;                       //Speed of the player
+
+    public CommandPointManager cpm;
+    public int playerNum = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        //Movement Code
+
+        if (Input.GetKey(controls[0]))
+        {
+            botRB.position += new Vector2 (0.0f, maxSpeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(controls[1]))
+        {
+            botRB.position += new Vector2(-maxSpeed, 0.0f) * Time.deltaTime;
+        }
+        if (Input.GetKey(controls[2]))
+        {
+            botRB.position += new Vector2(maxSpeed, 0.0f) * Time.deltaTime;
+        }
+        if (Input.GetKey(controls[3]))
+        {
+            botRB.position += new Vector2(0.0f, -maxSpeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(controls[4]))
+        {
+            interact();                                                                 
+        }
+
+    }
+
+
+
+    public void interact()                                                              //Holder meathod for later use
+    {
+        Debug.Log("Interact Key Pressed");
+    }
+
+
+    public void updateControls(string[] newControls)                                    //Set controls
+    {
+        int[] cpState = cpm.cpState;
+        for(int i = 0; i < 5; i++)
+        {
+            if (cpState[i] == playerNum)
+            {
+                controls[i] = desiredControls[i];
+            }
+            else
+            {
+                controls[i] = newControls[i];
+            }
+        }
+    }
+
+    public void setControlDesired(int cpNum)                                            //For setting controls when cp is captured
+    {
+        controls[cpNum] = desiredControls[cpNum];
+    }
+
+
+
+}
---
+++ Assets/Script/Randomizer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System;
+
+public class Randomizer : MonoBehaviour
+{
+    public GameObject blueBot;
+    public GameObject redBot;
+    public int test = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test == 1)
+        {
+            test = 0;
+            randomizerControls();
+        }
+    }
+
+    public void randomizerControls()
+    {
+        System.Random random = new System.Random();
+
+        List<string> controlHolder = new List<string>() { "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "f", "g", "h", "j", "k", "l", ";", "'","space", "z", "x", "c", "v", "b", "n", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
+        string[] blueControls = new string[5];
+        string[] redControls = new string[5];
+
+        int randomHolderVal = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            randomHolderVal = random.Next(0, controlHolder.Count);
+            blueControls[i] = controlHolder[randomHolderVal];
+            controlHolder.RemoveAt(randomHolderVal);
+
+            
+            randomHolderVal = random.Next(0, controlHolder.Count);
+            redControls[i] = controlHolder[randomHolderVal];
+            controlHolder.RemoveAt(randomHolderVal);
+     
+        }
+
+        //Change controls here
+        blueBot.GetComponent<BotControls>().updateControls(blueControls);
+        redBot.GetComponent<BotControls>().updateControls(redControls);
+
+    }
+
+
+}
